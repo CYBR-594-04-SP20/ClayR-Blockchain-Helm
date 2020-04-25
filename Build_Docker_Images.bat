f2bf21ec1d6ca157cc645ba0ce0042eb62140e74
Clay Rivetti <crivetti@sandiego.edu>
2020-04-25 12:34:34 -0700

    cleaned up project and added readme

---
+++ Build_Docker_Images.bat
@@ -0,0 +1,26 @@
+@echo off
+TITLE "Python Blockchain Docker Builder"
+
+echo.
+echo ----------------------------------------------------------
+echo -------Building Python Blockchain Node Docker Image-------
+echo ----------------------------------------------------------
+echo.
+
+pushd .\blockchain-helm\blockchain
+docker build -t blockchain-node .
+popd
+
+echo.
+echo ----------------------------------------------------------
+echo ------Building Python Blockchain Client Docker Image------
+echo ----------------------------------------------------------
+echo.
+
+pushd .\blockchain-helm\blockchain_client
+docker build -t blockchain-client .
+popd
+
+
+pause
+cmd \k
