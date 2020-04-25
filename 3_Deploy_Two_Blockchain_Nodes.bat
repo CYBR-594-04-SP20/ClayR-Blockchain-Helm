ac1d6d18cd32938c069523ea82038ca0f4ec46d5
Clay Rivetti <crivetti@sandiego.edu>
2020-04-25 13:36:11 -0700

    fixed comments in 3rd script

--- 3_Deploy_Two_Blockchain_Nodes.bat
+++ 3_Deploy_Two_Blockchain_Nodes.bat
@@ -9,6 +9,14 @@ echo.
 
 cd blockchain-helm
 helm install node1 blockchain-py
+
+echo.
+echo.
+echo ----------------------------------------------------------
+echo -------Deploying Blockchain Node2 Using Kubernetes--------
+echo ----------------------------------------------------------
+echo.
+
 helm install node2 blockchain-py --set service.nodePort=31002
 
 
